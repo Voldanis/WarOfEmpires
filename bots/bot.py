6ddffd56f7a4f604a26a61de08f0ba4d51c5dfa5
chartareginae <151648010+chartareginae@users.noreply.github.com>
2024-04-16 10:31:06 +0500

    Update bot.py

--- bots/bot.py
+++ bots/bot.py
@@ -13,35 +13,14 @@ class Bot(Example):
         self.not_processed_t = list()
 
     def move(self, response: dict):
-        reqs = [('equip', 't3')]
-        if len(response['player_units'].keys()) > 0:
-            unit = random.choice(list(response['player_units'].keys()))
-            road = random.choice(['r2', 'r6'])
-            reqs.append(('move', unit, road))
-        return reqs
-'''if response['status_code'] == 103 or response['status_code'] == 102 or response['status_code'] == 104:
-            self.flag = 1
-        if self.flag == 0:
-            self.response = response
-            self.not_processed_u = list(self.response['player_units'].keys())
-            self.not_processed_t = list(self.response['player_towns'].keys())
-            return 'equip', random.choice(list(self.response['player_towns'].keys()))
-        if self.flag == 1:
-            if len(self.not_processed_u) > 0:
-                unit = self.not_processed_u.pop(-1)
-                if self.response['player_units'][unit]['location'][0] == 't':
-                    if self.response['player_units'][unit]['location'] not in self.response['player_towns'].keys():
-                        return 'capture', unit
-                    else:
-                        return 'move', unit, random.choice(self.map_graph[self.response['player_units'][unit]['location']])
+        reqs = []
+        for town in response['player_towns'].keys():
+            reqs.append(('equip', town))
+        for unit in response['player_units'].keys():
+            if response['player_units'][unit]['location'][0] == 't':
+                if response['player_units'][unit]['location'] not in response['player_towns'].keys():
+                    reqs.append(('capture', unit))
                 else:
-                    return 'move', unit, 'aboba'
-            self.flag = 2
-        if self.flag == 2:
-            if len(self.not_processed_t) > 0:
-                town = self.not_processed_t.pop(-1)
-                return 'upgrade', town
-            self.flag = 0
-        return 'end'
-'''
-
+                    road = random.choice(list(self.map_graph[response['player_units'][unit]['location']]))
+                    reqs.append(('move', unit, road))
+        return reqs
